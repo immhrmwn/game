5a7dab222ecf2e0b0d56960b58673986addc8795
Imam Hermawan <immd.flores@gmail.com>
2022-09-06 12:13:09 +0700

    feat: created detail page with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,281 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- topbar -->
+    <div class="topBar">
+      <b-container fluid="sm">
+        <div class="flex alignCenter">
+          <h4 style="color: orange">BATGAME</h4>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <div id="topBarRight" class="flex gap24">
+            <div>
+              <span class="title" style="display: block;">Nomor Ponsel</span>
+              <span class="subtitle" style="display: block; font-weight: bold;">08123456789</span>
+            </div>
+            <div class="flex alignCenter gap">
+              <CurrencyDollarIcon  size="2x" style="color: silver;"/>
+              <div>
+                <span class="title" style="display: block; color: silver;">Token Platinum</span>
+                <span class="subtitle" style="display: block; font-weight: bold">1.000.000 Pt</span>
+              </div>
+              <b-button class="button title" style="font-weight: bold">TOP UP</b-button>
+            </div>
+            <div class="flex alignCenter gap">
+              <CurrencyDollarIcon  size="2x" style="color: gold;"/>
+              <div>
+                <span class="title" style="display: block; color: orange;">Winning Platinum</span>
+                <span class="subtitle" style="display: block; color: gold; font-weight: bold">1.000.000 Pt</span>
+              </div>
+              <b-button class="btnOrange title" style="font-weight: bold">TUKAR</b-button>
+            </div>
+          </div>
+        </div>
+      </b-container>
+    </div>
+    <!-- Nav -->
+    <Navbar />
+    <!-- Content -->
+    <b-container style="margin-bottom: 100px" >
+      <div class="content">
+        <h2>{{data.name}} <ShareIcon  size="0.75x" style="color: silver;"/></h2>
+        <div class="flex gap24" style="margin-bottom: 24px;">
+          <div class="media">
+            <b-img :src="data.thumbnail" fluid alt="Responsive image"></b-img>
+          </div>  
+          <div class="description">
+            <h3>Game description</h3>
+            <p>{{data.description}}</p>
+            <div>
+              <div style="display: flex; gap: 24px">
+                <b-button class="btnOrange" style="font-weight: bold">PLAY <CurrencyDollarIcon  size="1.5x"/>10</b-button>{{' '}}
+                <a :href="data.tutorial_game_url" target="_blank" style="color: inherit; font-size: 20px;">Play Demo</a>
+              </div>
+            </div>
+            </div>  
+        </div>
+        <div class="flex gap24">
+          <div class="media">
+          <h4>SCORE SYSTEM:</h4>
+          <div style="width: 100%; background: purple; min-height: 100px; padding: 20px; border-radius: 10px">
+            <b-table-simple small responsive>
+              <b-thead>
+                <b-tr class="table-text">
+                  <b-th colspan="1"><GiftIcon  size="1.5x"/> Score</b-th>
+                  <b-th colspan="1"><CurrencyDollarIcon style="color: orange"  size="1.5x"/> Winning Platinum Reward</b-th>
+                </b-tr>
+              </b-thead>
+              <b-tbody class="table-text">
+                <b-tr v-for="item in data.score_system" :key="item.id">
+                  <b-td>{{item.min}} - {{item.max}}</b-td>
+                  <b-td>{{item.reward}} Platinum</b-td>
+                </b-tr>
+              </b-tbody>
+            </b-table-simple>
+          </div>
+        </div>
+          <div style="min-width: 40%">
+            <h4>Rekomendasi Game lain:</h4>
+            <div class="recomendation shadow">
+              <b-img :src="data.thumbnail" fluid alt="Responsive image" style="border-radius: 10px"></b-img>
+              <div class="flex" style="padding: 8px">
+                <div>
+                  <h6>GAME TITLE</h6>
+                  <h6>MAX 2 ROW</h6>
+                </div>
+                <div>
+                  <span style="font-size: 14px">Lorem ipsum</span>
+                  <h6 style="color: orange"><CurrencyDollarIcon  size="1.5x"/>1.000<span style="font-size: 8px">Pt</span></h6>
+                </div>
+              </div>
+            </div>  
+            <div><a href="#" style="color: orangered; font-size: 24px; font-weight: 500;">LIHAT SEMUA GAME</a></div>
+          </div>
+        </div>
+      </div>
+    </b-container>
+    <!-- footer -->
+    <div class="footer">
+      <div class="flex alignCenter gap24">
+        <span class="batgame">BATGAME</span>
+        <span class="copyright">Copyright batgame.id ©. 2022 Rights Reserved</span>
+      </div>
+      <div class="flex gap24">
+        <div class="logo" />
+        <div class="logo" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar/Navbar.vue'
+import {  ShareIcon, CurrencyDollarIcon, GiftIcon } from '@vue-hero-icons/outline'
+import datas from '@/assets/game-detail'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      data: datas.data
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Navbar,
+    ShareIcon,
+    CurrencyDollarIcon,
+    GiftIcon
+  },
+  methods: {},
+  computed: {}
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  height: 100%;
+  background-image: url('@/assets/background.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.topBar {
+  background-color: rgba(1, 0, 85, 0.8);
+  color: white;
+  width: 100%;
+  padding: 10px;
+}
+.flex {
+  justify-content: space-between;
+  display: flex;
+}
+.alignCenter {
+  align-items: center;
+}
+.gap {
+  gap: 8px
+}
+.gap24 {
+  gap: 24px
+}
+.content {
+  background-color: rgba(1, 0, 85, 0.8);
+  box-shadow: 0 0 5px 5px purple;
+  border-radius: 8px;
+  margin-top: 24px;
+  color: white;
+  padding: 24px;
+}
+.button {
+  border-radius: 20px;
+  background-color: purple;
+  border: none;
+}
+.button:hover {
+  background-color: purple;
+  opacity: 0.8;
+}
+.btnOrange {
+  border-radius: 20px;
+  background-color: orange;
+  border: none;
+}
+.btnOrange:hover {
+  background-color: orange;
+  opacity: 0.8;
+}
+.title {
+  font-size: 12px;
+}
+.shadow {
+  box-shadow: 0 0 5px 5px purple;
+}
+.footer {
+  background-color: rgb(21, 0, 43);
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  padding: 20px;
+  color: white;
+}
+.batgame {
+  color: orange;
+  font-size: 28px;
+  font-weight: bold;
+}
+.logo {
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,114,1) 35%, rgba(255,0,41,1) 100%);
+  border-radius: 30px;
+  height: 60px;
+  width: 60px;
+}
+.media {
+  min-width: 532px;
+  width: 60%
+}
+.description {
+  min-width: 330px;
+}
+.table-text {
+  color: white;
+}
+.recomendation {
+  width: 60%;
+  min-width: 210px;
+  background: rgb(4, 0, 66);
+  border-radius: 10px;
+}
+
+@media only screen and (min-width: 2000px) {
+  .footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+}
+@media only screen and (max-width:1000px) {
+  .title {
+    font-size: 10px;
+  }
+  .subtitle {
+    font-size: 12px;
+  }
+  .flex {
+    flex-wrap: wrap;
+  }
+  .media {
+    min-width: 0;
+    width: 100%
+  }
+  .description {
+    min-width: 0;
+  }
+}
+@media only screen and (max-width: 770px) {
+  #topBarRight {
+    display: none;
+  }
+  .logo {
+    width: 30px;
+    height: 30px;
+  }
+  .batgame {
+    font-size: 20px;
+  }
+  .copyright {
+    font-size: 12px;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .table-text {
+    font-size: 10px;
+  }
+  .copyright {
+    display: none
+  }
 }
 </style>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" style="color: white;" class="bg">
+    <b-container fluid="sm">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#" @click="select = 'home'" :class="yellow('home')"><HomeIcon size="1x" /> Beranda</b-nav-item>
+        <b-nav-item href="#" @click="select = 'game'" :class="yellow('game')"><ColorSwatchIcon size="1x"/> Game</b-nav-item>
+        <b-nav-item href="#" @click="select = 'help'" :class="yellow('help')"><InformationCircleIcon size="1x"/> Bantuan</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-container>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+import { HomeIcon, ColorSwatchIcon, InformationCircleIcon } from "@vue-hero-icons/outline"
+
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      select: 'home',
+    }
+  },
+  components: {
+    HomeIcon,
+    ColorSwatchIcon,
+    InformationCircleIcon
+  },
+  methods: {
+    yellow: function(input) {
+      return this.select == input ? 'selected' : ''
+    }
+  },
+  computed: {},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .bg {
+    background: rgba(85, 0, 71, 0.9);
+  }
+  .nav-link {
+    align-items: center;
+    display: flex;
+    gap: 4px;
+  }
+  .navbar-dark {
+    --bs-navbar-color: white;
+  }
+  .selected {
+    border-bottom: 1px solid white;
+  }
+</style>
